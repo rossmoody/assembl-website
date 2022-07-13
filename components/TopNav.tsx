44b8f80466cce4ac5df73ed1a88458f95388a3b0
rossmoody <fiftyfivehis@gmail.com>
2022-07-13 13:11:04 -0700

    update

--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -1,17 +1,13 @@
 import { Box, Flex, Spacer } from '@chakra-ui/react'
-import { Fragment } from 'react'
 import Logotype from './Logotype'
 
 const TopNav = () => (
-  <Fragment>
-    <Flex as="nav" alignItems="center" py={8}>
-      <Box w="180px">
-        <Logotype />
-      </Box>
-      <Spacer />
-    </Flex>
-    <Spacer height={[8]} />
-  </Fragment>
+  <Flex as="nav" alignItems="center" py={8}>
+    <Box w="180px">
+      <Logotype />
+    </Box>
+    <Spacer />
+  </Flex>
 )
 
 export default TopNav
